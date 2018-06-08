4573d7ab74bc50ea2d7b0d1b9817078aefc644e6
Kaitlym <azkatie2014@gmail.com>
2018-06-08 20:43:47 +0000

    automatically backed up by learn

--- login.rb
+++ login.rb
@@ -13,6 +13,10 @@
 
 class User
   attr_accessor :name, :password, :email, :age
-  def initialize(name, password, email, age)
+  def initialize(username, password, email, age)
+  @username= username
+  @password = password
+  @email = email
+  @age = age
   end
 end
